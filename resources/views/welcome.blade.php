2ca0842e6ca84113a6fafde6571bb86a3a60b295
Yoshimitsu maegawa <panda.4432@icloud.com>
2021-06-18 14:21:56 +0000

    follow, unfollow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,8 @@
                 </div>
             </aside>
             <div class="col-sm-8">
+                 {{-- 投稿フォーム --}}
+                @include('microposts.form')
                 {{-- 投稿一覧 --}}
                 @include('microposts.microposts')
             </div>
